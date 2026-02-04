e2fe8a31d1948e09f06f8f340bacc973921328cd
Lucas Magalhães Rodrigues <127453899+iamlucasmagalhaes@users.noreply.github.com>
2026-02-04 00:01:59 -0300

    feat: add GetUsersController

---
+++ src/controllers/get-users/get-users.ts
@@ -0,0 +1,25 @@
+import type { IGetUsersController, IGetUsersRepository } from "./protocols.js";
+
+export class GetUsersController implements IGetUsersController{
+    getUsersRepository: IGetUsersRepository
+
+    constructor(getUsersRepository: IGetUsersRepository){
+        this.getUsersRepository = getUsersRepository
+    }
+    
+    async handle() {
+        try{
+            const users = await this.getUsersRepository.getUsers()
+ 
+            return {
+                statusCode: 200,
+                body: users
+            }
+        } catch (error){
+            return {
+                statusCode: 500,
+                body: "Something went wrong."
+            }
+        }
+    }
+}
